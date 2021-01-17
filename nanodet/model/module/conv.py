e237dda10226082738c4d90b7a561b66621a04a3
RangiLyu <lyuchqi@gmail.com>
2021-01-17 19:48:14 +0800

    Add RepVGG Conv Module

--- nanodet/model/module/conv.py
+++ nanodet/model/module/conv.py
@@ -1,7 +1,10 @@
 """
-from MMDetection
+ConvModule refers from MMDetection
+RepVGGConvModule refers from RepVGG: Making VGG-style ConvNets Great Again
 """
+import torch
 import torch.nn as nn
+import numpy as np
 import warnings
 from .init_weights import kaiming_init, normal_init, xavier_init, constant_init
 from .norm import build_norm_layer
@@ -217,3 +220,119 @@ class DepthwiseConvModule(nn.Module):
             elif layer_name == 'act' and self.activation:
                 x = self.act(x)
         return x
+
+
+class RepVGGConvModule(nn.Module):
+    """
+    RepVGG Conv Block from paper RepVGG: Making VGG-style ConvNets Great Again
+    https://arxiv.org/abs/2101.03697
+    https://github.com/DingXiaoH/RepVGG
+    """
+
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 kernel_size,
+                 stride=1,
+                 padding=0,
+                 dilation=1,
+                 groups=1,
+                 activation='ReLU',
+                 padding_mode='zeros',
+                 deploy=False):
+        super(RepVGGConvModule, self).__init__()
+        assert activation is None or isinstance(activation, str)
+        self.activation = activation
+
+        self.deploy = deploy
+        self.groups = groups
+        self.in_channels = in_channels
+
+        assert kernel_size == 3
+        assert padding == 1
+
+        padding_11 = padding - kernel_size // 2
+
+        # build activation layer
+        if self.activation:
+            self.act = act_layers(self.activation)
+
+        if deploy:
+            self.rbr_reparam = nn.Conv2d(in_channels=in_channels, out_channels=out_channels,
+                                         kernel_size=kernel_size, stride=stride,
+                                         padding=padding, dilation=dilation, groups=groups, bias=True,
+                                         padding_mode=padding_mode)
+
+        else:
+            self.rbr_identity = nn.BatchNorm2d(
+                num_features=in_channels) if out_channels == in_channels and stride == 1 else None
+
+            self.rbr_dense = nn.Sequential(nn.Conv2d(in_channels=in_channels, out_channels=out_channels,
+                                                     kernel_size=kernel_size, stride=stride, padding=padding,
+                                                     groups=groups, bias=False),
+                                           nn.BatchNorm2d(num_features=out_channels))
+
+            self.rbr_1x1 = nn.Sequential(nn.Conv2d(in_channels=in_channels, out_channels=out_channels,
+                                                   kernel_size=1, stride=stride, padding=padding_11,
+                                                   groups=groups, bias=False),
+                                         nn.BatchNorm2d(num_features=out_channels))
+            print('RepVGG Block, identity = ', self.rbr_identity)
+
+    def forward(self, inputs):
+        if hasattr(self, 'rbr_reparam'):
+            return self.act(self.rbr_reparam(inputs))
+
+        if self.rbr_identity is None:
+            id_out = 0
+        else:
+            id_out = self.rbr_identity(inputs)
+
+        return self.act(self.rbr_dense(inputs) + self.rbr_1x1(inputs) + id_out)
+
+    #   This func derives the equivalent kernel and bias in a DIFFERENTIABLE way.
+    #   You can get the equivalent kernel and bias at any time and do whatever you want,
+    #   for example, apply some penalties or constraints during training, just like you do to the other models.
+    #   May be useful for quantization or pruning.
+    def get_equivalent_kernel_bias(self):
+        kernel3x3, bias3x3 = self._fuse_bn_tensor(self.rbr_dense)
+        kernel1x1, bias1x1 = self._fuse_bn_tensor(self.rbr_1x1)
+        kernelid, biasid = self._fuse_bn_tensor(self.rbr_identity)
+        return kernel3x3 + self._pad_1x1_to_3x3_tensor(kernel1x1) + kernelid, bias3x3 + bias1x1 + biasid
+
+    def _pad_1x1_to_3x3_tensor(self, kernel1x1):
+        if kernel1x1 is None:
+            return 0
+        else:
+            return nn.functional.pad(kernel1x1, [1, 1, 1, 1])
+
+    def _fuse_bn_tensor(self, branch):
+        if branch is None:
+            return 0, 0
+        if isinstance(branch, nn.Sequential):
+            kernel = branch[0].weight
+            running_mean = branch[1].running_mean
+            running_var = branch[1].running_var
+            gamma = branch[1].weight
+            beta = branch[1].bias
+            eps = branch[1].eps
+        else:
+            assert isinstance(branch, nn.BatchNorm2d)
+            if not hasattr(self, 'id_tensor'):
+                input_dim = self.in_channels // self.groups
+                kernel_value = np.zeros((self.in_channels, input_dim, 3, 3), dtype=np.float32)
+                for i in range(self.in_channels):
+                    kernel_value[i, i % input_dim, 1, 1] = 1
+                self.id_tensor = torch.from_numpy(kernel_value).to(branch.weight.device)
+            kernel = self.id_tensor
+            running_mean = branch.running_mean
+            running_var = branch.running_var
+            gamma = branch.weight
+            beta = branch.bias
+            eps = branch.eps
+        std = (running_var + eps).sqrt()
+        t = (gamma / std).reshape(-1, 1, 1, 1)
+        return kernel * t, beta - running_mean * gamma / std
+
+    def repvgg_convert(self):
+        kernel, bias = self.get_equivalent_kernel_bias()
+        return kernel.detach().cpu().numpy(), bias.detach().cpu().numpy(),

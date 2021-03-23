87087dc36cce4f80df55e96475e8f3b7e2ddad5b
wwdok <wade.wang96@outlook.com>
2021-03-23 11:30:28 +0800

    prrevent overwrite previous training workspace folder

--- nanodet/util/path.py
+++ nanodet/util/path.py
@@ -6,3 +6,5 @@ from .rank_filter import rank_filter
 def mkdir(path):
     if not os.path.exists(path):
         os.makedirs(path)
+    else:
+        assert not os.path.exists(path), f"{path} already exists, in order not to overwrite this folder, please modify the save_dir in config yaml or delete this folder manually"

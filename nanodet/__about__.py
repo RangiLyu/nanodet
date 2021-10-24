f1f31268372e2c72588bf28813ae48a3c6a83413
RangiLyu <lyuchqi@gmail.com>
2021-10-24 18:49:55 +0800

    Add package info.

---
+++ nanodet/__about__.py
@@ -0,0 +1,24 @@
+import time
+
+_this_year = time.strftime("%Y")
+__version__ = "0.4.2"
+__author__ = "RangiLyu"
+__author_email__ = "lyuchqi@gmail.com"
+__license__ = "Apache-2.0"
+__copyright__ = f"Copyright (c) 2020-{_this_year}, {__author__}."
+__homepage__ = "https://github.com/RangiLyu/nanodet"
+
+__docs__ = (
+    "NanoDet: Deep learning object detection toolbox for super fast and "
+    "lightweight anchor-free object detection models."
+)
+
+__all__ = [
+    "__author__",
+    "__author_email__",
+    "__copyright__",
+    "__docs__",
+    "__homepage__",
+    "__license__",
+    "__version__",
+]
---
+++ nanodet/__init__.py
@@ -0,0 +1,8 @@
+"""package info."""
+
+import os
+
+from nanodet.__about__ import *  # noqa: F401 F403
+
+_PACKAGE_ROOT = os.path.dirname(__file__)
+_PROJECT_ROOT = os.path.dirname(_PACKAGE_ROOT)
